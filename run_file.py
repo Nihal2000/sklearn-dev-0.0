2efacec793c67c330e7a1091eaefe9ed6b41c2b4
vatsalmpatel <vatsalmpatel@gmail.com>
2021-03-23 15:45:50 +0530

    23/03

--- run_file.py
+++ run_file.py
@@ -1,62 +1,65 @@
-# python -m pip install --verbose --no-build-isolation --editable .
+# # python -m pip install --verbose --no-build-isolation --editable .
 
-import sys
-import subprocess
+# import sys
+# import subprocess
 
-#
-#  # implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--verbose','--no-build-isolation','--editable','.'])
+# #
+# #  # implement pip as a subprocess:
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--verbose','--no-build-isolation','--editable','.'])
 
-from sklearn.tree import LinearDecisionTreeRegressor, DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_regression
-
-
-#inp1 = int(input("Enter number of examples you want:"))
-m = 100
-np.random.seed(42)
-X = np.random.rand(m, 1)
-X= np.sort(X, axis= 0)
-y = 4 * (X[:, 0] - 0.5) ** 2 #+ 4 * (X[:, 1] - 0.5)**2 + 4 * (X[:, 2] - 0.5)**3
-y = y + np.random.randn(m,) / 10
-
-#X, y= make_regression(n_features= 4, random_state=42)
+# from sklearn.tree import LinearDecisionTreeRegressor, DecisionTreeRegressor
+# from sklearn.metrics import mean_squared_error
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.datasets import make_regression
 
 
-x_test= X[:20]
-y_test= y[:20]
+# #inp1 = int(input("Enter number of examples you want:"))
+# m = 100
+# np.random.seed(42)
+# X = np.random.rand(m, 1)
+# X= np.sort(X, axis= 0)
+# y = 4 * (X[:, 0] - 0.5) ** 2 #+ 4 * (X[:, 1] - 0.5)**2 + 4 * (X[:, 2] - 0.5)**3
+# y = y + np.random.randn(m,) / 10
 
-X= X[20:]
-y= y[20:]
+# #X, y= make_regression(n_features= 4, random_state=42)
 
-print("shape:", X.shape)
 
-inp2 = 2#int(input("Enter the Depth of the tree:"))
+# x_test= X[:20]
+# y_test= y[:20]
 
-ldtr = LinearDecisionTreeRegressor(max_depth = inp2)
-ldtr.fit(X,y)
+# X= X[20:]
+# y= y[20:]
 
-y_pred= ldtr.predict(X)
-print("error",mean_squared_error(y, y_pred), mean_squared_error(y_test, ldtr.predict(x_test)))
-ldtr = DecisionTreeRegressor(max_depth = inp2)
-ldtr.fit(X,y)
+# print("shape:", X.shape)
 
-y_pred1= ldtr.predict(X)
-print("error normal", mean_squared_error(y, y_pred1), mean_squared_error(y_test, ldtr.predict(x_test)))
+# inp2 = 2#int(input("Enter the Depth of the tree:"))
 
-plt.scatter(X[:, 0], y)
-plt.scatter(X[:, 0], y_pred)
-plt.scatter(X[:, 0], y_pred1)
-plt.show()
+# ldtr = LinearDecisionTreeRegressor(max_depth = inp2)
+# ldtr.fit(X,y)
 
-plt.scatter(X[:, 1], y)
-plt.scatter(X[:, 1], y_pred)
-plt.scatter(X[:, 1], y_pred1)
-plt.show()
+# y_pred= ldtr.predict(X)
+# print("error",mean_squared_error(y, y_pred), mean_squared_error(y_test, ldtr.predict(x_test)))
+# ldtr = DecisionTreeRegressor(max_depth = inp2)
+# ldtr.fit(X,y)
 
-plt.scatter(X[:, 2], y)
-plt.scatter(X[:, 2], y_pred)
-plt.scatter(X[:, 2], y_pred1)
-plt.show()
+# y_pred1= ldtr.predict(X)
+# print("error normal", mean_squared_error(y, y_pred1), mean_squared_error(y_test, ldtr.predict(x_test)))
+
+# plt.scatter(X[:, 0], y)
+# plt.scatter(X[:, 0], y_pred)
+# plt.scatter(X[:, 0], y_pred1)
+# plt.show()
+
+# plt.scatter(X[:, 1], y)
+# plt.scatter(X[:, 1], y_pred)
+# plt.scatter(X[:, 1], y_pred1)
+# plt.show()
+
+# plt.scatter(X[:, 2], y)
+# plt.scatter(X[:, 2], y_pred)
+# plt.scatter(X[:, 2], y_pred1)
+# plt.show()
+
+import sklearn
+print(sklearn)
